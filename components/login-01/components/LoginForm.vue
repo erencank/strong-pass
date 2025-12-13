4297be33b8d983ee0ab01dab594cc47075f82a65
Erencan Kanmaz <erencankanmaz@bol.com>
2025-12-13 18:21:43 +0100

    login form cleaned

--- components/login-01/components/LoginForm.vue
+++ components/login-01/components/LoginForm.vue
@@ -16,10 +16,44 @@ import {
   FieldLabel,
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import { toTypedSchema } from "@vee-validate/zod";
+import z from "zod";
+import { useForm, Field as VeeField } from "vee-validate";
+import { toast } from "vue-sonner";
 
 const props = defineProps<{
   class?: HTMLAttributes["class"];
 }>();
+
+const auth = useAuthStore();
+const isSubmitting = ref(false);
+
+const formSchema = toTypedSchema(
+  z.object({
+    email: z.email(),
+    password: z.string(),
+  })
+);
+
+const { handleSubmit, resetForm } = useForm({
+  validationSchema: formSchema,
+  initialValues: {
+    email: "",
+    password: "",
+  },
+});
+
+const onSubmit = handleSubmit(async (values, { setErrors }) => {
+  isSubmitting.value = true;
+  try {
+    await auth.login(values.email, values.password);
+  } catch (error: any) {
+    console.error("Login Failed:", error);
+    toast.error(error.message || "Login failed");
+  } finally {
+    isSubmitting.value = false;
+  }
+});
 </script>
 
 <template>
@@ -32,31 +66,52 @@ const props = defineProps<{
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <form>
+        <form id="login-form" @submit="onSubmit">
           <FieldGroup>
+            <VeeField v-slot="{ field, errors }" name="email">
+              <Field :data-invalid="!!errors.length">
+                <FieldLabel for="email"> Email </FieldLabel>
+                <Input
+                  v-bind="field"
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                  :aria-invalid="!!errors.length"
+                />
+                <FieldError v-if="errors.length" :errors="errors" />
+              </Field>
+            </VeeField>
+            <VeeField v-slot="{ field, errors }" name="password">
+              <Field :data-invalid="!!errors.length">
+                <div class="flex items-center">
+                  <FieldLabel for="password"> Password </FieldLabel>
+                  <a
+                    href="#"
+                    class="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <InputHidden
+                  v-model="password"
+                  v-bind="field"
+                  required
+                  :aria-invalid="!!errors.length"
+                />
+                <FieldError v-if="errors.length" :errors="errors" />
+              </Field>
+            </VeeField>
             <Field>
-              <FieldLabel for="email"> Email </FieldLabel>
-              <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
-            </Field>
-            <Field>
-              <div class="flex items-center">
-                <FieldLabel for="password"> Password </FieldLabel>
-                <a
-                  href="#"
-                  class="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                >
-                  Forgot your password?
-                </a>
-              </div>
-              <Input id="password" type="password" required aria-invalid="" />
-            </Field>
-            <Field>
-              <Button type="submit"> Login </Button>
+              <Button
+                type="submit"
+                form="login-form"
+                class="w-full"
+                :disabled="isSubmitting"
+              >
+                <Spinner v-if="isSubmitting" />
+                {{ isSubmitting ? "Logging in..." : "Login" }}
+              </Button>
               <FieldDescription class="text-center">
                 Don't have an account?
                 <NuxtLink to="/signup">Sign up</NuxtLink>
